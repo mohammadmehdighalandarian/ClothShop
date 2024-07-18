45c343d70402b2291d55aa4dc0738e054b59cd6a
mohammadmehdi <mohammadmehdighn@gmail.com>
2024-07-18 22:19:35 +0330

    Create Index Ui For Admin Pannel

--- ClothShop.Core/obj/Debug/net8.0/ClothShop.Core.AssemblyInfo.cs
+++ ClothShop.Core/obj/Debug/net8.0/ClothShop.Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClothShop.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08020afd5789015b0bc72a0f57589c0ef7fa85f4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+287898bf92382e874b779bdb5eff404245d5558a")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClothShop.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClothShop.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- ClothShop.DataLayer/obj/Debug/net8.0/ClothShop.DataLayer.AssemblyInfo.cs
+++ ClothShop.DataLayer/obj/Debug/net8.0/ClothShop.DataLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClothShop.DataLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08020afd5789015b0bc72a0f57589c0ef7fa85f4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+287898bf92382e874b779bdb5eff404245d5558a")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClothShop.DataLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClothShop.DataLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
