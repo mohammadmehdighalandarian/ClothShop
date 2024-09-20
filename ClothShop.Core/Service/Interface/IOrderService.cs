2beb15873fa1d4f15c286fe458b062e53eb9a1b5
mohammadmehdi <mohammadmehdighn@gmail.com>
2024-09-20 20:30:28 +0330

    Last commit after End Project

--- ClothShop.Core/Service/Interface/IOrderService.cs
+++ ClothShop.Core/Service/Interface/IOrderService.cs
@@ -1,39 +1,36 @@
 ﻿using ClothShop.Core.DTOs.Order;
 using ClothShop.DataLayer.Entities.Order;
 
-namespace ClothShop.Core.Service.Interface
+namespace ClothShop.Core.Service.Interface;
+
+public interface IOrderService
 {
-   public interface IOrderService
-   {
-       int AddOrder(string userName, int ProductId);
+    int AddOrder(string userName, int ProductId);
 
-       void UpdatePriceOrder(int orderId);
+    void UpdatePriceOrder(int orderId);
 
-       Order GetOrderForUserPanel(string userName, int orderId);
-       Order GetOrderById(int orderId);
+    Order GetOrderForUserPanel(string userName, int orderId);
+    Order GetOrderById(int orderId);
 
-       bool FinalyOrder(string userName,int orderId);
+    bool FinalyOrder(string userName,int orderId);
 
-       List<Order> GetUserOrders(string userName);
+    List<Order> GetUserOrders(string userName);
 
-       void UpdateOrder(Order order);
+    void UpdateOrder(Order order);
 
-     
+    #region DisCount
 
-       #region DisCount
+    DiscountUseType UseDiscount(int orderId, string code);
 
-       DiscountUseType UseDiscount(int orderId, string code);
+    void AddDiscount(Discount discount);
 
-       void AddDiscount(Discount discount);
+    List<Discount> GetAllDiscounts();
 
-       List<Discount> GetAllDiscounts();
+    Discount GetDiscountById(int discountId);
 
-       Discount GetDiscountById(int discountId);
+    void UpdateDiscount(Discount discount);
 
-       void UpdateDiscount(Discount discount);
+    bool IsExistCode(string code);
 
-       bool IsExistCode(string code);
-
-       #endregion
-   }
+    #endregion
 }
